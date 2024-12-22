810036da862c54c76e7d1e1b4dd2589c2cff4f60
sfsajid91 <sfsajid91@gmail.com>
2024-12-22 09:50:48 +0600

    refactor: streamline browser initialization and ensure proper closure in fetchData

--- src/scraper/scrape-data.ts
+++ src/scraper/scrape-data.ts
@@ -2,31 +2,16 @@ import puppeteer, { Browser } from "puppeteer";
 
 export class ScraperError extends Error {}
 
-let browser: Browser | null = null;
-
-const initializeBrowser = async () => {
-  if (browser) {
-    return browser;
-  }
-  browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  return browser;
-};
-
-// const closeBrowser = async () => {
-//   if (browser) {
-//     await browser.close();
-//     browser = null;
-//   }
-// };
-
 export const fetchData = async (query: string) => {
+  let browser: Browser | null = null;
   try {
-    const browser = await initializeBrowser();
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     const formattedQuery = encodeURIComponent(query);
+
     await page.goto(`https://duckduckgo.com/?q=${formattedQuery}`);
 
     await page.waitForSelector('a[data-testid="result-title-a"]');
@@ -41,5 +26,9 @@ export const fetchData = async (query: string) => {
   } catch (error) {
     console.error(error);
     throw new ScraperError("Failed to fetch data");
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 };
