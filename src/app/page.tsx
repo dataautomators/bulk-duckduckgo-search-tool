fda27588e14a25fce3f666daf0fd82b2c7747d65
ardhrubo <ardhrubo3038@gmail.com>
2024-12-28 11:47:13 +0600

    fix: updated header section with custom text and made responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,35 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="container mx-auto px-0.5 py-6 flex-grow flex flex-col items-center">
+      <div className="container mx-auto px-0.5 py-6 flex-grow flex flex-col items-center sm-400:mb-2 sm:mb-4 md:mb-6">
         
-        <header className="w-full py-4 mb-7">
+        <header className="w-full py-4 mb-4">
           <div className="container mx-auto px-2">
             <div className="flex flex-col items-center">
               <div className="grid items-center text-center justify-center">
-              <div className="space-y-1">
+              <div className="space-y-2">
                   <h1 className="text-lg font-bold md:text-2xl lg:text-3xl xl:text-4xl">
                     Bulk DuckDuckGo Search Tool
                   </h1>
                 </div>
-                <div className="flex justify-end items-center space-x-2 text-muted-foreground">
-                  <span className="text-sm">Powered by 
+                <div className="flex justify-end py-1 items-center text-lg xs:text-xs sm-400:text-sm text-muted-foreground space-x-2">
+                  <span className="">Powered by 
                   <a
                     href="https://scrapeautomate.com/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="font-semibold text-sm text-blue-500 hover:text-blue-500/80 transition-colors duration-200"
+                    className="font-semibold text-blue-500 hover:text-blue-500/80 transition-colors duration-200"
                   > ScrapeAutomate</a>
                   </span>
                   <p>
                   |
                   </p>
-                  <span className="text-sm"> Built By 
+                  <span className=""> Built By 
                   <a
                     href="https://scrapeautomate.com/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="font-semibold text-sm text-blue-500 hover:text-blue-500/80 transition-colors duration-200"
+                    className="font-semibold text-blue-500 hover:text-blue-500/80 transition-colors duration-200"
                   > DataAutomators
                   </a>
                   </span>
